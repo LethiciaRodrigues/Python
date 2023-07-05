1b2de9b144d2868bd2b4414cf1174b7315ba5f3a
Nadasei <yuzuaihara2807@gmail.com>
2023-07-05 09:27:51 -0300

    1° desafio
    1° exercício de python

---
+++ desafio01.py
@@ -0,0 +1,2 @@
+n = input ('Qual o seu nome ?')
+print ('Olá, seja bem vindo ' , n)
---
+++ desafio02.py
@@ -0,0 +1,4 @@
+d = input ('Digite o dia em que nasceu ')
+m = input ('Digite o mês em que nasceu')
+a = input ('Digite o ano em que nasceu ')
+print ('Você nasceu no dia', d, m, a, ',correto?')
