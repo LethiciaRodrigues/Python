653df2c02d4c0bc48339cc39a4ed0449149bfe89
Nadasei <yuzuaihara2807@gmail.com>
2023-07-11 11:06:08 -0300

    melhorando o código

--- desafio04.py
+++ desafio04.py
@@ -1,3 +1,11 @@
-print('Progra leitor')
+print('Programa leitor')
 a=input ('Digite algo:')
-print('O valor que você digitou é',type(a))
+print('O que você digitou possui valor:',type(a))
+print('Possui espaços?',a.isspace())
+print('Possui números? ',a.isnumeric())
+print('É alfabetico?',a.isalpha())
+print('É alfanúmerico?',a.isalnum())
+print('Possui letras maiúsculas?',a.isupper())
+print('Possui letras minúsculas ?',a.islower())
+
+
