3cbd8d1a01b8ab4ffe165632663d7c8a8d014c08
Lethicia Loyse <yuzuaihara2807@gmail.com>
2023-12-11 11:47:48 -0300

    Update desafio11.py

--- desafio11.py
+++ desafio11.py
@@ -1,2 +1,5 @@
 print('Pintando uma parede')
-(input(''))
+l=float(input('Por favor, digite a largura da parede:')) 
+h=float(input('Por favor, digite a altura da parede:'))
+b = h * l
+print(f'A parede possui as dimensões de {l}X{h} e a aréa é de metros {b}')
