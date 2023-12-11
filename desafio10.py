6228ba5f85d952c221f54e908eabf537bb0550e6
Lethicia Loyse <yuzuaihara2807@gmail.com>
2023-12-11 10:58:42 -0300

    Create desafio10.py
    Programa usado para conversão de moedas.

---
+++ desafio10.py
@@ -0,0 +1,6 @@
+print('Conversor de moedas')
+r = float(input('Qual o valor que você possui na carteira? R$'))
+d = r / 4.89
+e = r / 0.19
+print(f'Com R${r :.2f}, você pode comprar US${d :.2f} ou se preferir, €{e :.2f}!')
+           
