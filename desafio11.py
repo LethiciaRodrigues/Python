6aeadac56be300e63f8d42fa211a51467c4832e8
Lethicia Loyse <yuzuaihara2807@gmail.com>
2023-12-11 14:21:34 -0300

    Finalizando o código
    Aplicando a lógica dentro do programa.

--- desafio11.py
+++ desafio11.py
@@ -2,4 +2,6 @@ print('Pintando uma parede')
 l=float(input('Por favor, digite a largura da parede:')) 
 h=float(input('Por favor, digite a altura da parede:'))
 b = h * l
-print(f'A parede possui as dimensões de {l}X{h} e a aréa é de metros {b}')
+print(f'A parede possui as dimensões de {l}X{h} e a aréa é {b} m²')
+t= b/2 
+print(f'Para pintar essa parede, você precisará de {t}L de tinta')
---
+++ desafio12.py
@@ -0,0 +1,4 @@
+print('Calculando descontos')
+p = float(input('Qual o valor atual do produto? R$'))
+d = p - (p * 5 / 100)
+print(f'O produto que custava R${p :.2f}, com 5% de desconto vai custar R$ {d :.2f} .')
