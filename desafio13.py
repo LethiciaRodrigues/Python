60ad3a46d646e49b9e89177104b749284fcd251c
Lethicia Loyse <yuzuaihara2807@gmail.com>
2023-12-12 19:59:35 -0300

    Create desafio13.py
    Treinando exercícios com porcentagem.

---
+++ desafio13.py
@@ -0,0 +1,4 @@
+print('Reajsute salarial')
+s = float(input('Qual é o salário atual do funcionário? R$'))
+n = s  + (s * 15 / 100) 
+print(f'O funcionário que ganhava {s :.2f}, com o aumento de 15%, ele passa a receber {n :.2f}')
